dc3e81f6d5733f47fa7d639fde11ec9f9cdb7841
LeoBerti <leoberti@gmail.com>
2019-11-06 12:41:30 -0300

    DeprecationWarning mongo solved

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,8 @@
 import Sequelize from 'sequelize';
-
+import mongoose from 'mongoose';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
-
 import databaseConfig from '../config/database';
 
 const models = [User, File, Appointment];
@@ -11,6 +10,7 @@ const models = [User, File, Appointment];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -19,6 +19,17 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber',
+      {
+        useNewUrlParser: true, // estou utilizando um formato novo na string de conexão
+        useFindAndModify: true, // para poder buscar e atualizar os registros
+        useUnifiedTopology: true, // DeprecationWarning apareceu no console então eu estou usando, conforme a recomendação do mongo
+      }
+    );
   }
 }
 
